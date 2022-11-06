b28b2801d75ebd56e07e60bc342da2339314759d
Santiago Arrieta <santiagoarve@unisabana.edu.co>
2022-11-05 19:33:05 -0500

    correcion microservicios

--- src/main/java/co/edu/unisabana/HboMax/Consumidor.java
+++ src/main/java/co/edu/unisabana/HboMax/Consumidor.java
@@ -1,5 +1,6 @@
 package co.edu.unisabana.HboMax;
 
+import co.edu.unisabana.peliculas.controller.Pelicula;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -9,9 +10,9 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class Consumidor {
 
-    @RabbitListener(queues = {"unisabana.peliculas.agregar"})
-    public void receive(@Payload String mensaje) {
-        log.info("Escuche de la cola unisabana.peliculas.agregar:" , mensaje);
+    @RabbitListener(queues = {"unisabana.peliculas.hbomax"})
+    public void receive(@Payload Pelicula mensaje) {
+        log.info("la pelicula publicada es {}" , mensaje.getNombrePelicula());
     }
 
 
